bcb50c014ca11b50a64169efb5daed25b9edf0c7
Olivier <ducrouxolivier@gmail.com>
2025-02-22 21:49:05 +0100

    deploy

---
+++ db/production.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS software_markets_production;
+CREATE DATABASE IF NOT EXISTS software_markets_production_cache;
+CREATE DATABASE IF NOT EXISTS software_markets_production_queue;
+CREATE DATABASE IF NOT EXISTS software_markets_production_cable;
