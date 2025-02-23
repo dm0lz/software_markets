761e933b979a0f9dbab5569b347e89d2d46fcad5
Olivier <ducrouxolivier@gmail.com>
2025-02-24 00:57:39 +0100

    create FetchWebPageContentJob

--- app/jobs/scrap_domain_job.rb
+++ app/jobs/scrap_domain_job.rb
@@ -2,10 +2,13 @@ class ScrapDomainJob < ApplicationJob
   queue_as :default
 
   def perform(domain)
-    output, error, status = BrowsePageService.new("http://#{domain.name.sub("https://", "")}", "{}").call(js_code)
+    output, error, status = BrowsePageService.new("http://#{domain.name}", "{}").call(js_code)
     if status.success?
       results = JSON.parse(output)
-      domain.web_pages.create!(url: results["url"], content: results["content"])
+      domain.web_pages.find_or_create_by!(url: results["url"], content: results["content"])
+      results["links"].each do |link|
+        FetchWebPageContentJob.perform_later(link)
+      end
       logger.info "Successfully scraped domain #{domain.name}"
     else
       logger.error "Error: #{error.strip}"
@@ -17,7 +20,12 @@ class ScrapDomainJob < ApplicationJob
     <<-JS
       return {
         url:  document.location.href,
-        content: document.body.innerText
+        content: document.body.innerText,
+        links: [...new Set(
+          [...document.querySelectorAll('a[href]')]
+            .map(a => a.href)
+            .filter(href => href.startsWith(location.origin))
+        )];
       }
     JS
   end
