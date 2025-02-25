b191a0e76e9360e8ae11a088141fe307791f9db8
Olivier <ducrouxolivier@gmail.com>
2025-02-25 16:25:20 +0100

    typo

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,7 +2,7 @@ class Domain < ApplicationRecord
   validates :name, presence: true
   belongs_to :company
   has_many :software_applications, dependent: :destroy
-  has_many :web_pages, depedent: :destroy
+  has_many :web_pages, dependent: :destroy
   has_many :keyword_web_pages, dependent: :destroy
   has_many :keywords, through: :keyword_web_pages
 
