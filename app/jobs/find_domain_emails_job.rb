44cf07fc29b4d311423a04d396aca97943cb09a8
Olivier <ducrouxolivier@gmail.com>
2025-03-01 17:41:22 +0100

    renaming FindDomainEmailsService

--- app/jobs/find_domain_emails_job.rb
+++ app/jobs/find_domain_emails_job.rb
@@ -2,7 +2,7 @@ class FindDomainEmailsJob < ApplicationJob
   queue_as :default
 
   def perform(domain_name, session_id)
-    emails = DomainEmailsFinderService.new.call(domain_name)
+    emails = FindDomainEmailsService.new.call(domain_name)
     ActionCable.server.broadcast(
       "domain_emails_finder_#{session_id}",
       { message: emails }
--- app/services/find_domain_emails_service.rb
+++ app/services/find_domain_emails_service.rb
@@ -1,4 +1,4 @@
-class DomainEmailsFinderService < BaseService
+class FindDomainEmailsService < BaseService
   def call(domain_name)
     serp_web_pages = FetchSerpWebPagesService.new.call("inbody%3A*%40#{domain_name}")
     serp_web_pages.scan(/\b[A-Za-z0-9._%+-]+@#{Regexp.escape(domain_name)}\b/i).uniq rescue nil
