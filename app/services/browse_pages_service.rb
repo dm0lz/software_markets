620b4b5a96c43c48217111ca4d881b0648fd2114
Olivier <ducrouxolivier@gmail.com>
2025-03-01 12:03:13 +0100

    run concurrent requests in BrowsePagesService

--- app/services/browse_pages_service.rb
+++ app/services/browse_pages_service.rb
@@ -22,24 +22,24 @@ class BrowsePagesService < BaseService
       (async () => {
         const browser = await firefox.launch(#{@options});
         const results = [];
-        for (const url of #{@urls}) {
-          try {
-            const page = await browser.newPage();
-            await page.goto(url, { timeout: 10000, waitUntil: "domcontentloaded" });
-            const data = await Promise.race([
-              page.evaluate(() => {
-                #{script}
-              }),
-              new Promise((_, reject) =>
-                setTimeout(() => reject(new Error("Evaluate Timeout")), 10000)
-              )
-            ]);
-            results.push(data);
-            await page.close();
-          } catch (error) {
-            console.error(`Error scraping ${url}:`, error);
-          }
-        }
+        await Promise.all(
+          #{@urls}.map(async(url) => {
+            try {
+              const page = await browser.newPage();
+              await page.goto(url, { timeout: 10000, waitUntil: "domcontentloaded" });
+              const data = await Promise.race([
+                page.evaluate(() => {
+                  #{script}
+                }),
+                new Promise((_, reject) =>
+                  setTimeout(() => reject(new Error("Evaluate Timeout")), 10000)
+                )
+              ]);
+              results.push(data);
+              await page.close();
+            } catch (error) {}
+          })
+        );
         console.log(JSON.stringify(results));
         await browser.close();
       })();
