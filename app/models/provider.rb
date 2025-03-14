50e0dbad80912f54274449ee68ed3931be6d1ef1
Olivier <ducrouxolivier@gmail.com>
2025-03-14 12:04:00 +0100

    provider model tests

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ApplicationRecord
-  validates :name, :domain, presence: true
+  validates :name, :domain, presence: true, uniqueness: true
   has_many :market_providers, dependent: :destroy
   has_many :markets, through: :market_providers
 end
--- test/models/provider_test.rb
+++ test/models/provider_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class ProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @provider = build(:provider)
+  end
+
+  test "factory is valid" do
+    assert @provider.valid?
+  end
+
+  test "name should be present" do
+    @provider.name = ""
+    assert_not @provider.valid?
+    assert_includes @provider.errors[:name], "can't be blank"
+  end
+
+  test "domain should be present" do
+    @provider.domain = ""
+    assert_not @provider.valid?
+    assert_includes @provider.errors[:domain], "can't be blank"
+  end
+
+  test "name should be unique" do
+    create(:provider, name: "UniqueName")
+    duplicate = build(:provider, name: "UniqueName")
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:name], "has already been taken"
+  end
+
+  test "domain should be unique" do
+    create(:provider, domain: "example.com")
+    duplicate = build(:provider, domain: "example.com")
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:domain], "has already been taken"
+  end
 end
