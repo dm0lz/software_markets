bf5e17c0ee361178b83f325fe7601d71a059f9c9
Olivier <ducrouxolivier@gmail.com>
2025-03-15 22:06:34 +0100

    deploy config

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 require "openai"
 OpenAI.configure do |config|
   config.access_token = "not-needed"
-  config.uri_base = "http://#{ENV['OLLAMA_HOST']}:11434/v1" || "http://localhost:11434/v1"
+  config.uri_base = "http://#{ENV.fetch("OLLAMA_IP") { "localhost" }}:11434/v1"
   config.log_errors = true # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
 end
